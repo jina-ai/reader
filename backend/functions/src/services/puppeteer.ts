f4f189c8e6de900bf8ac4a9b2d70fcc7a3834c53
yanlong.wang <yanlong.wang@naiver.org>
2024-08-01 19:51:53 +0800

    fix

--- backend/functions/src/services/puppeteer.ts
+++ backend/functions/src/services/puppeteer.ts
@@ -126,7 +126,10 @@ function getMaxDepthAndCountUsingTreeWalker(root) {
   const treeWalker = document.createTreeWalker(
     root,
     NodeFilter.SHOW_ELEMENT,
-    (node) => (node.nodeName.toLowerCase() === 'svg' || node.nodeName.toLowerCase() === 'code') ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_ACCEPT,
+    (node) => {
+      const nodeName = node.nodeName.toLowerCase();
+      return (nodeName === 'svg' || nodeName === 'code') ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_ACCEPT;
+    },
     false
   );
 
