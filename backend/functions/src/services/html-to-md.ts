b3647492a871c5cad1190f003a1b2cf0212792c8
yanlong.wang <yanlong.wang@naiver.org>
2024-09-11 18:38:03 +0800

    wip

--- backend/functions/src/services/html-to-md.ts
+++ backend/functions/src/services/html-to-md.ts
@@ -1,14 +1,8 @@
 import { AsyncService, AutoCastable, Prop } from 'civkit';
 import { Logger } from '../shared/services/logger';
 
-const pMdAstToMarkdown = import('mdast-util-to-markdown');
-const pUnistVisitParents = import('unist-util-visit-parents');
-
 
 export class HTMLtoMarkdown extends AsyncService {
-
-    mdAstToMarkdown!: Awaited<typeof pMdAstToMarkdown>;
-    unistVisitParents!: Awaited<typeof pUnistVisitParents>;
 
     constructor(
         protected logger: Logger,
@@ -19,9 +13,6 @@ export class HTMLtoMarkdown extends AsyncService {
 
     override async init() {
         await this.dependencyReady();
-
-        this.mdAstToMarkdown = await pMdAstToMarkdown;
-        this.unistVisitParents = await pUnistVisitParents;
 
         this.emit('ready');
     }
@@ -49,7 +40,7 @@ class MDCode extends MarkdownASTNode {
         required: true,
         default: ''
     })
-    value!: string;
+    text!: string;
 }
 
 class MDHTML extends MarkdownASTNode {
@@ -62,7 +53,7 @@ class MDHTML extends MarkdownASTNode {
         required: true,
         default: ''
     })
-    value!: string;
+    text!: string;
 }
 
 
@@ -75,6 +66,21 @@ class MarkdownASTParentNode extends MarkdownASTNode {
     children!: MarkdownASTNode[];
 }
 
+
+class MarkdownASTRoot extends MarkdownASTParentNode {
+    @Prop({
+        default: 'root'
+    })
+    override type!: 'root';
+}
+
+class MDParagraph extends MarkdownASTParentNode {
+    @Prop({
+        default: 'paragraph'
+    })
+    override type!: 'paragraph';
+}
+
 class MDHeading extends MarkdownASTParentNode {
     @Prop({
         default: 'heading'
@@ -85,11 +91,12 @@ class MDHeading extends MarkdownASTParentNode {
         required: true,
         type: Number,
         validate(v: number) {
-            return v >=1 && v<=6
+            return v >= 1 && v <= 6;
         }
     })
-    level!: 1|2|3|4|5|6;
+    level!: 1 | 2 | 3 | 4 | 5 | 6;
 }
+
 class MDList extends MarkdownASTParentNode {
     @Prop({
         default: 'list'
@@ -100,6 +107,87 @@ class MDList extends MarkdownASTParentNode {
         default: false,
     })
     ordered!: boolean;
+}
+
+class MDListItem extends MarkdownASTParentNode {
+    @Prop({
+        default: 'listItem'
+    })
+    override type!: 'listItem';
+
+    @Prop({
+        default: false,
+    })
+    spread?: boolean;
+
+    @Prop()
+    checked?: boolean;
+
+    @Prop({
+        default: false,
+        arrayOf: MarkdownASTNode
+    })
+    override children!: MarkdownASTNode[];
+}
+
+class MDLink extends MarkdownASTParentNode {
+    @Prop({
+        default: 'link'
+    })
+    override type!: 'link';
+
+    @Prop({
+        required: true
+    })
+    href!: string;
+
+    @Prop()
+    alt?: string;
+
+    @Prop({
+        default: false,
+        arrayOf: MarkdownASTNode
+    })
+    override children!: MarkdownASTNode[];
+}
+
+class MDStrong extends MarkdownASTParentNode {
+    @Prop({
+        default: 'strong'
+    })
+    override type!: 'strong';
+
+    @Prop({
+        default: false,
+        arrayOf: MarkdownASTNode
+    })
+    override children!: MarkdownASTNode[];
+}
+
+class MDEmphasis extends MarkdownASTParentNode {
+    @Prop({
+        default: 'emphasis'
+    })
+    override type!: 'emphasis';
+
+    @Prop({
+        default: false,
+        arrayOf: MarkdownASTNode
+    })
+    override children!: MarkdownASTNode[];
+}
+
+class MDDelete extends MarkdownASTParentNode {
+    @Prop({
+        default: 'delete'
+    })
+    override type!: 'delete';
+
+    @Prop({
+        default: false,
+        arrayOf: MarkdownASTNode
+    })
+    override children!: MarkdownASTNode[];
 }
 
 
@@ -113,11 +201,164 @@ class MDLiteral extends MarkdownASTNode {
         required: true,
         default: ''
     })
-    value!: string;
+    text!: string;
+}
+
+class MDLineBreak extends MarkdownASTNode {
+    @Prop({
+        default: 'break'
+    })
+    override type!: 'break';
+}
+
+class MDThematicBreak extends MarkdownASTNode {
+    @Prop({
+        default: 'thematicBreak'
+    })
+    override type!: 'thematicBreak';
+}
+
+class MDImage extends MarkdownASTNode {
+    @Prop({
+        default: 'image'
+    })
+    override type!: 'image';
+
+    @Prop({
+        required: true
+    })
+    src!: string;
+
+    @Prop()
+    alt?: string;
+
+    @Prop()
+    title?: string;
+}
+
+class MDInlineCode extends MarkdownASTNode {
+    @Prop({
+        default: 'inlineCode'
+    })
+    override type!: 'inlineCode';
+
+    @Prop({
+        required: true,
+        default: ''
+    })
+    text!: string;
+}
+
+class MDMath extends MarkdownASTNode {
+    @Prop({
+        default: 'math'
+    })
+    override type!: 'math';
+
+    @Prop()
+    lang?: string;
+
+    @Prop({
+        required: true,
+        default: ''
+    })
+    text!: string;
+}
+
+class MDInlineMath extends MarkdownASTNode {
+    @Prop({
+        default: 'inlineMath'
+    })
+    override type!: 'inlineMath';
+
+    @Prop()
+    lang?: string;
+
+    @Prop({
+        required: true,
+        default: ''
+    })
+    text!: string;
 }
 
 
+class MDTableHeading extends MarkdownASTNode {
+    @Prop({
+        default: 'tableHeading'
+    })
+    override type!: 'tableHeading';
 
+    @Prop({
+        required: true
+    })
+    text!: string;
+
+    @Prop({
+        default: 'left',
+        validate(v: string) {
+            return ['left', 'center', 'right'].includes(v);
+        }
+    })
+    align?: 'left' | 'center' | 'right';
+}
+
+class MDTableHeader extends MarkdownASTParentNode {
+    @Prop({
+        default: 'tableHeader'
+    })
+    override type!: 'tableHeader';
+
+    @Prop({
+        default: false,
+        arrayOf: MDTableHeading
+    })
+    override children!: MDTableHeading[];
+}
+class MDTableCell extends MarkdownASTParentNode {
+    @Prop({
+        default: 'tableCell'
+    })
+    override type!: 'tableCell';
+
+    @Prop({
+        default: false,
+        arrayOf: MarkdownASTNode
+    })
+    override children!: MarkdownASTNode[];
+}
+
+class MDTableRow extends MarkdownASTParentNode {
+    @Prop({
+        default: 'tableRow'
+    })
+    override type!: 'tableRow';
+
+    @Prop({
+        default: false,
+        arrayOf: MDTableCell
+    })
+    override children!: MDTableCell[];
+}
+
+class MDTable extends MarkdownASTParentNode {
+    @Prop({
+        default: 'table'
+    })
+    override type!: 'table';
+
+    @Prop({
+        default: false,
+        arrayOf: [MDTableHeader, MDTableRow]
+    })
+    override children!: (MDTableHeader | MDTableRow)[];
+}
+
+class MDBlockQuote extends MarkdownASTParentNode {
+    @Prop({
+        default: 'blockquote'
+    })
+    override type!: 'blockquote';
+}
 
 export class HTMLToMarkdownJob {
 
