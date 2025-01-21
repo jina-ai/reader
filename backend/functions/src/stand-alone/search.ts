6e9aa976dc2e09446a917e3914c458ffb35be6e9
yanlong.wang <yanlong.wang@naiver.org>
2025-01-21 11:42:12 +0800

    listen port from env

--- backend/functions/src/stand-alone/search.ts
+++ backend/functions/src/stand-alone/search.ts
@@ -148,4 +148,4 @@ const instance = container.resolve(SearchStandAloneServer);
 
 export default instance;
 
-instance.serviceReady().then((s) => s.h2c().listen(3000));
+instance.serviceReady().then((s) => s.h2c().listen(parseInt(process.env.PORT || '') || 3000));
