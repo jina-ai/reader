5b7dfd130f3d93488365198d2b68fa3735038df0
Yanlong Wang <yanlong.wang@naiver.org>
2025-03-07 13:52:54 +0800

    fix: pptr declare ready only when page can be created without issues

--- backend/functions/src/services/puppeteer.ts
+++ backend/functions/src/services/puppeteer.ts
@@ -528,9 +528,9 @@ export class PuppeteerControl extends AsyncService {
         this.logger.info(`Browser launched: ${this.browser.process()?.pid}, ${this.ua}`);
         this.curlControl.impersonateChrome(this.ua.replace(/Headless/i, ''));
 
-        this.emit('ready');
+        await this.newPage().then((r) => this.__loadedPage.push(r));
 
-        this.newPage().then((r) => this.__loadedPage.push(r));
+        this.emit('ready');
     }
 
     @perNextTick()
