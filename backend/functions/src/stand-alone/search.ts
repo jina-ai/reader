6b0cf7467a6f692759555a67e8076b95def9f1b1
Yanlong Wang <yanlong.wang@naiver.org>
2025-03-07 10:56:33 +0800

    fix: no h2c for searcher

--- backend/functions/src/stand-alone/search.ts
+++ backend/functions/src/stand-alone/search.ts
@@ -136,4 +136,4 @@ const instance = container.resolve(SearchStandAloneServer);
 
 export default instance;
 
-instance.serviceReady().then((s) => s.h2c().listen(parseInt(process.env.PORT || '') || 3000));
+instance.serviceReady().then((s) => s.listen(parseInt(process.env.PORT || '') || 3000));
