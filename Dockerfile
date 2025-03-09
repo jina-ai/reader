5337faec01e5c0d683855138fc5f3f4e0f49d3b7
Yanlong Wang <yanlong.wang@naiver.org>
2025-03-09 22:45:04 +0800

    wip: try to leverage NODE_COMPILE_CACHE

--- Dockerfile
+++ Dockerfile
@@ -29,8 +29,11 @@ COPY licensed ./licensed
 
 RUN rm -rf ~/.config/chromium && mkdir -p ~/.config/chromium
 
+RUN NODE_COMPILE_CACHE=node_modules npm run dry-run
+
 ENV OVERRIDE_CHROME_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
 ENV LD_PRELOAD=/usr/local/lib/libcurl-impersonate.so CURL_IMPERSONATE=chrome116 CURL_IMPERSONATE_HEADERS=no
+ENV NODE_CACHE_DIR=node_modules
 ENV PORT=8080
 
 EXPOSE 3000 3001 8080 8081
--- src/services/puppeteer.ts
+++ src/services/puppeteer.ts
@@ -1,4 +1,3 @@
-import os from 'os';
 import fs from 'fs';
 import { container, singleton } from 'tsyringe';
 import { AsyncService, Defer, marshalErrorLike, AssertionFailureError, delay, Deferred, perNextTick, ParamValidationError, FancyFile } from 'civkit';
@@ -474,7 +473,7 @@ export class PuppeteerControl extends AsyncService {
         protected blackHoleDetector: BlackHoleDetector,
     ) {
         super(...arguments);
-        this.setMaxListeners(2 * Math.floor(os.totalmem() / (256 * 1024 * 1024)) + 1); 148 - 95;
+        this.setMaxListeners(Infinity);
 
         let crippledTimes = 0;
         this.on('crippled', () => {
