cf01a2c5047a9955d83ddb9faca31ae33c651bdf
yanlong.wang <yanlong.wang@naiver.org>
2025-03-10 15:17:42 +0800

    chore: add comments to clarify

--- src/api/crawler.ts
+++ src/api/crawler.ts
@@ -228,6 +228,8 @@ export class CrawlerHost extends RPCHost {
         let chargeAmount = 0;
         const crawlerOptions = ctx.method === 'GET' ? crawlerOptionsHeaderOnly : crawlerOptionsParamsAllowed;
 
+        // ctx.url in Koa is still not a real URL, just the full path including search query.
+        // ctx.path in Koa does not include the search query.
         const targetUrl = await this.getTargetUrl(tryDecodeURIComponent(ctx.url), crawlerOptions);
         if (!targetUrl) {
             return await this.getIndex(auth);
