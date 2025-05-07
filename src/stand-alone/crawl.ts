a9fe036ddc54b0a1d0d77191f8ab7482a64d7c2d
Yanlong Wang <yanlong.wang@naiver.org>
2025-05-07 18:24:19 +0800

    fix: set maxConcurrentStreams for http2 so it can be load-balanced reasonably

--- src/stand-alone/crawl.ts
+++ src/stand-alone/crawl.ts
@@ -44,6 +44,10 @@ export class CrawlStandAloneServer extends KoaServer {
             const ar = new AsyncResource('HTTP2ServerRequest');
             ar.runInAsyncScope(fn, this.koaApp, req, res);
         });
+        // HTTP2 p2p may break the load balancing
+        this.httpServer.updateSettings({
+            maxConcurrentStreams: 8,
+        });
         // useResourceBasedDefaultTracker();
 
         return this;
